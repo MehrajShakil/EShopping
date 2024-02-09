e782e50dd7511e7722305cbb41abd7d2e421518a
Mehraj <sisakilcste18@gmail.com>
2024-02-09 23:34:12 +0600

    working on navbar component

---
+++ Client/eshopping/src/app/Components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss',
+  //encapsulation: ViewEncapsulation.ShadowDom
+})
+export class NavBarComponent {
+
+}
